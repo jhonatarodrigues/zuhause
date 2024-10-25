32b8b427b666a193e51d477bb01effdb373f60b8
Jhonata Rodrigues <jhonata.rodrigues@syngenta.com>
2024-10-25 14:19:58 -0300

    feat: remove string por on product

--- src/components/BoxOffer/BoxOffer.tsx
+++ src/components/BoxOffer/BoxOffer.tsx
@@ -42,7 +42,7 @@ const BoxOffer = ({product}: BoxOfferProps) => {
           <div className="title">{product.title.rendered}</div>
           <div className="price">{product.acf.preco}</div>
           {product.acf.novo_preco && (
-            <div className="newPrice">por: <strong>{product.acf.novo_preco}</strong></div>
+            <div className="newPrice"><strong>{product.acf.novo_preco}</strong></div>
           )}
           <a href={`https://wa.me/+${process.env.REACT_APP_WHATHSAPP_NUMBER}?text=estou interessado no produto ${product.title.rendered}`} target='_blank' rel="noreferrer">
             <Button title="Fale com um consultor" />
